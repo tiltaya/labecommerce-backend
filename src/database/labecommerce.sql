489b2da8363a4fd988bab3d760bd2738685038e2
tiltaya <114702626+tiltaya@users.noreply.github.com>
2023-06-21 21:53:09 -0300

    Exercícios Aprofundamento SQL e Update READ.ME

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -31,3 +31,38 @@ VALUES ("prod001", "Mouse gamer", 250, "Melhor mouse do mercado!", "https://pics
 ("prod003", "Headset gamer", 550, "Headset 7.1 surround", "https://images.kabum.com.br/produtos/fotos/63196/63196_1518113965_index_g.jpg"),
 ("prod004", "Microfone de mesa", 470, "Microfone condensador USB", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa4nJn1uuGbTAKaNOpralj_aLieTUEuxDeMw&usqp=CAU"),
 ("prod005", "Webcam", 420, "Webcam Full HD 4k com microfone", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRad91dLPzPhkaT74d2F3Qq4oIZdw19IR-ZYw&usqp=CAU");
+
+-- Get All Users
+SELECT * FROM users;
+
+-- Get All Products (funcionalidade 1)
+SELECT * FROM products;
+
+-- Get All Products (funcionalidade 2)
+SELECT * FROM products
+WHERE name LIKE "%gamer%";
+
+-- Create User
+INSERT INTO users (id, name, email, password, created_at)
+VALUES ("u004", "Johannes", "johannes@labenu.com.br", "johannes123", "21/06/2023");
+
+-- Create Product
+INSERT INTO products (id, name, price, description, image_url)
+VALUES ("prod006", "Cadeira gamer", 1350, "Cadeira ergonômica com luzes RGB", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ-v5XGUwRH79552vEe3Vti0JfHy8Vx8qyqg&usqp=CAU");
+
+-- Delete user by id
+DELETE FROM users
+WHERE id = "u004";
+
+-- Delete product by id
+DELETE FROM products
+WHERE id = "prod006";
+
+-- Edit product by id
+UPDATE products
+SET id = "4060",
+    name = "test",
+    price = 5000,
+    description = "test description",
+    image_url = "test url"
+WHERE id = "prod006"
